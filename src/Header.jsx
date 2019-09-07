0ed5dd313646edd1fe3a1c1b33cb611e55822f34
DmitriTar <https://github.com/Dmiyri/todoList.git>
2019-09-08 00:48:49 +0300

    social network start

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+const Header = () => {
+	return (
+		<div >
+			<img src='https://st2.depositphotos.com/3867453/7605/v/950/depositphotos_76055207-stock-illustration-letter-s-logo-icon-design.jpg'/>
+		</div>
+	)
+}
+export default Header;
